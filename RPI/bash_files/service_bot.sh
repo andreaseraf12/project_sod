e716bad7538fc1eafc203a2fd5f9196df00ffc74
andreaseraf12 <101402881+andreaseraf12@users.noreply.github.com>
2024-06-01 20:54:53 +0200

    Add files via upload

---
+++ RPI/bash_files/service_bot.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+sleep 10
+source /home/rpi/progett_sod/bin/activate
+sleep 5
+python3 /home/rpi/chatbot.py
---
+++ RPI/bash_files/service_db.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+sleep 5
+source /home/rpi/progett_sod/bin/activate
+sleep 10
+python3 /home/rpi/database.py
